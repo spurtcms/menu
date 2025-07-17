2e89c5c3a2490e4913f92da313e73066cae6596a
spurtcmsdeveloper-146 <119719731+parvesh-piccosoft@users.noreply.github.com>
2025-07-17 12:16:00 +0530

    templatelist changes

--- templates.go
+++ templates.go
@@ -2,14 +2,14 @@ package menu
 
 import "fmt"
 
-func (menu *Menu) GoTemplatesList(isdeleted int) (goTemplateList []TblGoTemplates, count int64, err error) {
+func (menu *Menu) GoTemplatesList(tenantid string) (goTemplateList []TblGoTemplates, count int64, err error) {
 
 	if AuthError := AuthandPermission(menu); AuthError != nil {
 
 		return []TblGoTemplates{}, 0, AuthError
 	}
 
-	list, count, err := menumodel.ListGoTemplates(isdeleted, menu.DB)
+	list, count, err := menumodel.ListGoTemplates(tenantid, menu.DB)
 
 	if err != nil {
 
@@ -21,13 +21,13 @@ func (menu *Menu) GoTemplatesList(isdeleted int) (goTemplateList []TblGoTemplate
 	return list, count, nil
 }
 
-func (menu *Menu) GetTemplateById(id int,tenantid string) (TblGoTemplates, error) {
+func (menu *Menu) GetTemplateById(id int, tenantid string) (TblGoTemplates, error) {
 
 	if AuthError := AuthandPermission(menu); AuthError != nil {
 
 		return TblGoTemplates{}, AuthError
 	}
-	template, err := menumodel.GetTemplateById(id,tenantid ,menu.DB)
+	template, err := menumodel.GetTemplateById(id, tenantid, menu.DB)
 
 	if err != nil {
 
@@ -35,4 +35,20 @@ func (menu *Menu) GetTemplateById(id int,tenantid string) (TblGoTemplates, error
 
 	}
 	return template, nil
+}
+
+func (menu *Menu) CloneTemplatesBySlug(slug string, tenantid string, userid int,usertype string) error {
+
+	if AuthError := AuthandPermission(menu); AuthError != nil {
+
+		return AuthError
+	}
+	err := menumodel.CloneTemplatesBySlug(menu.DB, slug, tenantid, userid,usertype)
+
+	if err != nil {
+
+		return err
+
+	}
+	return nil
 }
--- templatesmodel.go
+++ templatesmodel.go
@@ -2,27 +2,41 @@ package menu
 
 import (
 	"fmt"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type TblGoTemplates struct {
-	Id            int
-	TemplateName  string
-	TemplateImage string
-	IsDeleted     int
-	TenantId      string
+	Id                 int
+	TemplateName       string
+	TemplateImage      string
+	IsDeleted          int
+	TenantId           string
+	CreatedBy          int
+	ChannelSlugName    string
+	TemplateModuleName string
+	CreatedOn          time.Time
 }
 
-func (menu *MenuModel) ListGoTemplates(isdeleted int, DB *gorm.DB) (list []TblGoTemplates, count int64, err error) {
+func (menu *MenuModel) ListGoTemplates(tenantid string, DB *gorm.DB) (list []TblGoTemplates, count int64, err error) {
 
 	var GoTemplatesList []TblGoTemplates
 
-	if err := DB.Table("tbl_go_templates").Where("is_deleted=?", isdeleted).Find(&GoTemplatesList).Count(&count).Error; err != nil {
+	if tenantid != "" {
 
-		return []TblGoTemplates{}, 0, err
+		if err := DB.Table("tbl_go_templates").Where("is_deleted=0 and tenant_id=?", tenantid).Find(&GoTemplatesList).Count(&count).Error; err != nil {
+
+			return []TblGoTemplates{}, 0, err
+		}
+
+	} else {
+
+		if err := DB.Table("tbl_go_templates").Where("is_deleted=0 and tenant_id is null").Find(&GoTemplatesList).Count(&count).Error; err != nil {
+
+			return []TblGoTemplates{}, 0, err
+		}
 	}
-
 	fmt.Println("Hello")
 
 	return GoTemplatesList, count, nil
@@ -36,4 +50,67 @@ func (menu *MenuModel) GetTemplateById(moduleid int, tenantid string, DB *gorm.D
 	}
 
 	return templates, nil
+}
+
+func (menu *MenuModel) CloneTemplatesBySlug(db *gorm.DB, slug string, tenantID string, userid int, usertype string) error {
+	var templates []TblGoTemplates
+
+	switch usertype {
+
+	case "new":
+
+		if err := db.Debug().Where("channel_slug_name = ? AND is_deleted = 0 AND tenant_id IS NULL", slug).Find(&templates).Error; err != nil {
+			return fmt.Errorf("error fetching templates for new user: %w", err)
+		}
+
+	case "old":
+
+		var count int64
+		if err := db.Model(&TblGoTemplates{}).Where("channel_slug_name = ? AND tenant_id = ? AND is_deleted = 0", slug, tenantID).Count(&count).Error; err != nil {
+			return fmt.Errorf("error checking existing old user templates: %w", err)
+		}
+
+		if count > 0 {
+
+			return nil
+		}
+
+		if err := db.Debug().Where("channel_slug_name = ? AND is_deleted = 0 AND tenant_id IS NULL", slug).Find(&templates).Error; err != nil {
+			return fmt.Errorf("error fetching global templates for old user: %w", err)
+		}
+
+	default:
+		return fmt.Errorf("invalid userType: must be 'new' or 'old'")
+	}
+
+	if len(templates) == 0 {
+		return fmt.Errorf("no templates found to clone for slug: %s", slug)
+	}
+
+	var newTemplates []TblGoTemplates
+	now := time.Now().UTC()
+
+	for _, t := range templates {
+
+		t.Id = 0
+		t.TenantId = tenantID
+		t.CreatedOn = now
+		t.CreatedBy = userid
+		newTemplates = append(newTemplates, t)
+
+	}
+
+	if err := db.Create(&newTemplates).Error; err != nil {
+		return fmt.Errorf("error inserting templates: %w", err)
+	}
+	var template TblGoTemplates
+	if err := db.Debug().Where("is_deleted = 0 AND tenant_id=? and created_by=?", tenantID, userid).First(&template).Error; err != nil {
+		return fmt.Errorf("error fetching templates for new user: %w", err)
+	}
+
+	if err := db.Debug().Table("tbl_users").Where("is_deleted = 0 AND tenant_id = ? AND id = ?", tenantID, userid).Updates(map[string]interface{}{"go_template_default": template.Id}).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
