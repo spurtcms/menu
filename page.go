e088fd740c5f9314849e75ecfbbcea62041f282b
spurtcmsdeveloper-146 <119719731+parvesh-piccosoft@users.noreply.github.com>
2025-12-19 19:02:52 +0530

    create page func changes

--- page.go
+++ page.go
@@ -2,7 +2,6 @@ package menu
 
 import (
 	"fmt"
-	"strings"
 	"time"
 )
 
@@ -36,7 +35,7 @@ func (menu *Menu) CreateTemplatePage(page *TblTemplatePages) (TblTemplatePages, 
 
 		return TblTemplatePages{}, AuthError
 	}
-	page.Slug = strings.ToLower(strings.ReplaceAll(page.Name, " ", "-"))
+	// page.Slug = strings.ToLower(strings.ReplaceAll(page.Name, " ", "-"))
 	page.CreatedOn, _ = time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
 	pagedetail, err := menumodel.CreateTemplatePage(menu.DB, page)
 
