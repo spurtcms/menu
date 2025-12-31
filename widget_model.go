fff53428589108b208b4b360c448becc6fcff79f
spurtcmsdeveloper-146 <119719731+parvesh-piccosoft@users.noreply.github.com>
2025-12-31 19:16:38 +0530

    changes of widget channel

--- widget_model.go
+++ widget_model.go
@@ -37,11 +37,11 @@ type TblWidgets struct {
 	ListingData           []listing.TblListing         `gorm:"-"`
 	CategoryBaseEntryData []channels.Tblchannelentries `gorm:"-"`
 
-	WidgetTitle     string                       `gorm:"-:migration;<-:false"`
-	WidgetId        int                          `gorm:"-:migration;<-:false"`
-	WidgetLimit     int                          `gorm:"type:integer"`
-	PageData        []TblTemplatePages           `gorm:"-"`
-	ChennalsEntries []channels.Tblchannelentries `gorm:"-"`
+	WidgetTitle     string                `gorm:"-:migration;<-:false"`
+	WidgetId        int                   `gorm:"-:migration;<-:false"`
+	WidgetLimit     int                   `gorm:"type:integer"`
+	PageData        []TblTemplatePages    `gorm:"-"`
+	ChennalsEntries []channels.TblChannel `gorm:"-"`
 }
 
 type TblWidgetProducts struct {
@@ -313,44 +313,22 @@ func (menu1 *MenuModel) FetchWidgetPages(DB *gorm.DB, widgetID int, input Widget
 	return pages, err
 
 }
-func (menu1 *MenuModel) FetchWidgetchennals(DB *gorm.DB, widgetID int, input WidgetInput) ([]channels.Tblchannelentries, error) {
 
-	var channels_entries []channels.Tblchannelentries
+func (menu1 *MenuModel) FetchWidgetchennals(DB *gorm.DB, widgetID int, input WidgetInput) ([]channels.TblChannel, error) {
+
+	var channels_entries []channels.TblChannel
 
 	query := DB.Debug().
-		Table("tbl_channel_entries AS ce").
-		Select(`
-        ce.*,
-        c.slug_name AS channel_name
-    `).
-		Joins("JOIN tbl_widget_products AS wp ON wp.product_id = ce.channel_id").
-		Joins("LEFT JOIN tbl_channels AS c ON c.id = ce.channel_id").
-		Where(`
-			wp.widget_id = ? 
-			AND ce.is_deleted = 0 
-			AND ce.is_active = 1
- 
-		`, widgetID)
+		Table("tbl_channels AS ce").
+		Select("ce.*").
+		Joins("JOIN tbl_widget_products AS wp ON wp.product_id = ce.id").
+		Where("wp.widget_id = ? AND ce.is_deleted = 0 AND ce.is_active = 1", widgetID)
 
 	if input.Limit > 0 {
 		query = query.Limit(input.Limit)
 	}
 
-	if input.NoDirectAccess {
-
-		query = query.Debug().Where("ce.access_type <> ?", "no_direct_access")
-
-	}
-
-	if !input.Profile {
-
-		query = query.Where("ce.access_type = ? OR ce.access_type IS NULL", "every_one")
-	}
-
-	if input.MemberRoleId != 2 {
-		query = query.Where("ce.user_role_id = ? OR ce.user_role_id = 0", 1)
-	}
-
 	err := query.Find(&channels_entries).Error
+
 	return channels_entries, err
 }
