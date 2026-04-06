174c9d7b692c0c2be315dc4696079c02c23ac52a
piccotalent216-hash <piccotalent216@gmail.com>
2026-04-06 19:54:51 +0530

    site setting flow update

--- settingsmodel.go
+++ settingsmodel.go
@@ -1,9 +1,6 @@
 package menu
 
 import (
-	"errors"
-	"fmt"
-
 	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
@@ -44,106 +41,29 @@ func (menu *MenuModel) SettingDetail(tenantid string, websiteid int, DB *gorm.DB
 
 func (menu *MenuModel) SettingsUpdates(settingsdetails TblGoTemplateSettings, DB *gorm.DB) (err error) {
 
-	var settinglist TblGoTemplateSeo
+	if settingsdetails.SiteName != "" {
+		if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"site_name": settingsdetails.SiteName, "site_logo": settingsdetails.SiteLogo, "site_logo_path": settingsdetails.SiteLogoPath, "site_fav_icon": settingsdetails.SiteFavIcon, "site_fav_icon_path": settingsdetails.SiteFavIconPath, "website_url": settingsdetails.WebsiteUrl, "template_type": settingsdetails.TemplateType, "header_thame": settingsdetails.HeaderThame}).Error; err != nil {
 
-	fmt.Println("hello", settingsdetails.TemplateType)
-
-	result := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).First(&settinglist)
-
-	if result.Error != nil {
-
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-
-			if err := DB.Table("tbl_go_template_settings").Create(&settingsdetails).Error; err != nil {
-
-				return err
-			}
-
-			fmt.Println("chechthiscondition")
-			if settingsdetails.WebsiteUrl != "" {
-
-				if err := DB.Debug().Table("tbl_websites").Where("tenant_id = ? and id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"name": settingsdetails.WebsiteUrl}).Error; err != nil {
-
-					return err
-				}
-
-			}
-
-			return nil
-
-		} else {
-
-			fmt.Println("Database error:", result.Error)
+			return err
 
 		}
 
-	} else {
+	}
+	if len(settingsdetails.SocialMediaLink) != 0 {
+		if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"social_media_link": settingsdetails.SocialMediaLink}).Error; err != nil {
 
-		if settingsdetails.SiteName != "" {
-
-			if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"site_name": settingsdetails.SiteName}).Error; err != nil {
-
-				return err
-
-			}
-
-		} else if settingsdetails.SiteLogo != "" {
-
-			if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"site_logo": settingsdetails.SiteLogo, "site_logo_path": settingsdetails.SiteLogoPath}).Error; err != nil {
-
-				return err
-			}
-
-		} else if settingsdetails.SiteFavIcon != "" {
-
-			if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"site_fav_icon": settingsdetails.SiteFavIcon, "site_fav_icon_path": settingsdetails.SiteFavIconPath}).Error; err != nil {
-
-				return err
-			}
-
-		} else if settingsdetails.WebsiteUrl != "" {
-
-			if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"website_url": settingsdetails.WebsiteUrl}).Error; err != nil {
-
-				return err
-			}
-
-			if err := DB.Table("tbl_websites").Where("tenant_id = ? and id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"name": settingsdetails.WebsiteUrl}).Error; err != nil {
-
-				return err
-			}
-
-		} else if string(settingsdetails.TemplateType) != "" && string(settingsdetails.TemplateType) != "null" && string(settingsdetails.TemplateType) != "[]" {
-			fmt.Println("dskfdskjf")
-
-			if err := DB.Table("tbl_go_template_settings").Debug().
-				Where("tenant_id = ? AND website_id = ?", settingsdetails.TenantId, settingsdetails.WebsiteId).
-				Update("template_type", settingsdetails.TemplateType).Error; err != nil {
-
-				return err
-			}
-		} else if string(settingsdetails.SocialMediaLink) != "" && string(settingsdetails.SocialMediaLink) != "null" && string(settingsdetails.SocialMediaLink) != "[]" {
-
-			if err := DB.Table("tbl_go_template_settings").Debug().
-				Where("tenant_id = ? AND website_id = ?", settingsdetails.TenantId, settingsdetails.WebsiteId).
-				Update("social_media_link", settingsdetails.SocialMediaLink).Error; err != nil {
-
-				return err
-			}
-
-		} else if settingsdetails.HeaderThame != "" {
-
-			if err := DB.Table("tbl_go_template_settings").Debug().
-				Where("tenant_id = ? AND website_id = ?", settingsdetails.TenantId, settingsdetails.WebsiteId).
-				Update("header_thame", settingsdetails.HeaderThame).Error; err != nil {
-
-				return err
-			}
+			return err
 
 		}
 
-		return nil
+	}
 
+	if settingsdetails.TemplateType != nil && settingsdetails.WebsiteUrl != "" {
+		if err := DB.Table("tbl_go_template_settings").Where("tenant_id = ? and website_id=?", settingsdetails.TenantId, settingsdetails.WebsiteId).UpdateColumns(map[string]interface{}{"template_type": settingsdetails.TemplateType, "website_url": settingsdetails.WebsiteUrl}).Error; err != nil {
+
+			return err
+
+		}
 	}
 
 	return nil
